3bfcb24eea7ba11db28e1187401f99b0cddd992b
MotiRR <motigullin.ra@yandex.ru>
2019-09-25 22:35:47 +0300

    Changed access modifiers from public to private, initialization of startTime, timeSpent. Handling 0 and negative numbers.

--- src/main/java/ru.sber.edu/Main.java
+++ src/main/java/ru.sber.edu/Main.java
@@ -1,3 +1,5 @@
+package ru.sber.edu;
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -9,16 +11,17 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public final static BigInteger[][] ONE = {{BigInteger.ZERO, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ONE}};
+    private final static BigInteger[][] ONE = {{BigInteger.ZERO, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ONE}};
 
-    public static BigInteger[][] mul(BigInteger[][] a, BigInteger[][] b) {
+    private static BigInteger[][] mul(BigInteger[][] a, BigInteger[][] b) {
         BigInteger[][] res = {
                 {a[0][0].multiply(b[0][0]).add(a[0][1].multiply(b[1][0])), a[0][0].multiply(b[0][1]).add(a[0][1].multiply(b[1][1]))},
                 {a[1][0].multiply(b[0][0]).add(a[1][1].multiply(b[1][0])), a[1][0].multiply(b[0][1]).add(a[1][1].multiply(b[1][1]))}
         };
         return res;
     }
-    public static BigInteger[][] pow(BigInteger[][] a, long k) {
+
+    private static BigInteger[][] pow(BigInteger[][] a, long k) {
 
         if (k == 0) return ONE;
         if (k == 1) return a;
@@ -27,31 +30,39 @@ public class Main {
         return pow(pow(a, k / 2), 2);
     }
 
-    public static void main(String args[])
+    public static void main(String[] arg)
     {
         BigInteger[][] rez;
         long n = 0L;
-        long startTime = 0L;
-        long timeSpent = 0L;
+        long startTime, timeSpent;
 
         startTime = System.nanoTime();
 
-        if(args.length == 0)
+        if(arg.length == 0)
         {
             Scanner sc = new Scanner(System.in);
+            System.out.println("Note. The number of elements will be converted to an absolute number.");
             System.out.println("Enter the number of Fibonacci elements");
             try {
                 n = sc.nextLong();
             }
             catch (Exception ex)
             {
-                System.out.println("Invalid input");
-                return;
+                System.err.println(String.format("Invalid input Error: %s", ex));
+                System.exit(-1);
             }
         }
         else {
-            n = new Long(args[0]);
+            n = new Long(arg[0]);
         }
+
+        if(n == 0)
+        {
+            System.out.println("Number of Fibonacci elements equal 0");
+            return;
+        }
+        else
+            n = Math.abs(n);
 
         for (long i = 1L ; i <= n; i = i + 3)
         {
